a294700e0723df0b078cc40949504fe307f3c924
제주 제주 <qwert6603@naver.com>
2022-01-03 00:30:27 +0900

    chapter01

--- chapter01.py
+++ chapter01.py
@@ -175,3 +175,154 @@ for i in range(a, b+1):
 print(f'{a}부터 {b}까지 정수의 합은 {sum}입니다.')
 '''
 
+# 반복 + 조건
+'''
+print('a부터 b까지 정수의 합을 구합니다.')
+a = int(input('정수 a를 입력하세요.: '))
+b = int(input('정수 b를 입력하세요.: '))
+
+if a > b:
+    a, b = b, a
+
+sum = 0
+for i in range(a, b+1):
+    if i < b:
+        print(f'{i} + ', end='')
+    else:
+        print(f'{i} = ', end='')
+    sum += i
+print(sum)
+'''
+'''
+print('a부터 b까지 정수의 합을 구합니다.')
+a = int(input('정수 a를 입력하세요.: '))
+b = int(input('정수 b를 입력하세요.: '))
+
+if a > b:
+    a, b = b, a
+
+sum = 0
+for i in range(a, b):
+    print(f'{i} + ', end='')
+    sum += i
+
+print(f'{b} = ', end='')
+sum += b
+print(sum)
+'''
+
+# 반복+조건 2
+'''
+print('+와 -를 번갈아 출력합니다.')
+n = int(input('몇 개를 출력할까요?: '))
+
+for i in range(n):
+    if i % 2:
+        print('-', end='')
+    else:
+        print('+', end='')
+print()
+'''
+'''
+print('+와 -를 번갈아 출력합니다.')
+n = int(input('몇 개를 출력할까요?: '))
+
+for i in range(1, n+1):
+    if i % 2:
+        print('+', end='')
+    else:
+        print('-', end='')
+print()
+'''
+'''
+print('+와 -를 번갈아 출력합니다.')
+n = int(input('몇 개를 출력할까요?: '))
+
+for _ in range(n // 2):
+    print('+-', end='')
+
+if n % 2:
+    print('+', end='')
+
+print()
+'''
+
+# 반복+조건 3
+'''
+print('*를 출력합니다.')
+n = int(input('몇 개를 출력할까요: '))
+w = int(input('몇 개마다 줄바꿈할까요: '))
+
+for i in range(n):
+    print('*', end='')
+    if i % w == w -1:
+        print()
+
+if n % w:
+    print()
+'''
+'''
+print('*를 출력합니다.')
+n = int(input('몇 개를 출력할까요?: '))
+w = int(input('몇 개마다 줄바꿈할까요?: '))
+
+for _ in range(n // w):
+    print('*' * w)
+
+rest = n % w
+if rest:
+    print('*' * rest)
+'''
+'''
+# 양수만 입력받기
+print('1부터 n까지의 정수의 합을 구합니다.')
+
+while True:
+    n = int(input('n값을 입력하세요.: '))
+    if n > 0:
+        break
+sum = 0
+i = 1
+for i in range(1, n+1):
+    sum += i
+    i += 1
+
+print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+'''
+
+# 직사각형 넓이로 변의 길이 구하기
+'''
+area = int(input('직사각형의 넓이를 입력하세요.:'))
+for i in range(1,area+1):
+    if i*i > area: break
+    if area % i: continue
+    print(f'{i} x {area // i}')
+'''
+
+# for_else
+'''
+import random
+
+n = int(input('난수의 개수를 입력하세요.: '))
+
+for _ in range(n):
+    r = random.randint(10, 99)
+    print(r, end=' ')
+    if r == 13:
+        print('\n프로그램을 중단합니다.')
+        break
+else:
+    print('\n난수 생성을 종료합니다.')
+'''
+
+# 반복문 건너뛰기와 여러 범위 스캔하기
+for i in range(1, 13):
+    if i == 8:
+        continue
+    print(i, end=' ')
+print()
+
+
+for i in list(range(1,8)) + list(9,13):
+    print(i, end='')
+print()
