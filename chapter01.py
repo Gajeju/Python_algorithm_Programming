db8c8631afaf535d53b0c17720dd66aac742a3a3
제주 제주 <qwert6603@naver.com>
2022-01-02 15:50:03 +0900

    chapter01

--- chapter01.py
+++ chapter01.py
@@ -129,6 +129,7 @@ else:
 '''
 
 # 반복
+'''
 print ('1부터 n까지 정수의 합을 구합니다.')
 n = int(input('n값을 입력하세요.: '))
 
@@ -139,5 +140,38 @@ while i <= n:
     sum += i
     i += 1
 print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+'''
+'''
+print('1부터 n까지 정수의 합을 구합니다.')
+n = int(input('n값을 입력하세요.: '))
 
+sum = 0
+for i in range(1, n+1):
+    sum += i
+
+print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+'''
+'''
+print('1부터 n까지 정수의 합을 구합니다.')
+n = int(input('n값을 입력하세요.: '))
+
+sum = n * (n + 1) // 2
+
+print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+'''
+'''
+# 연속하는 정수으 합 구하기
+print('a부터 b까지 정수의 합을 구합니다.')
+a = int(input('정수 a를 입력하세요.: '))
+b = int(input('정수 b를 입력하세요.: '))
+
+if a > b:
+    a, b = b, a
+
+sum = 0
+for i in range(a, b+1):
+    sum += i
+
+print(f'{a}부터 {b}까지 정수의 합은 {sum}입니다.')
+'''
 
