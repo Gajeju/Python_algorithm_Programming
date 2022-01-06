9d05764090eaf3c1d493492d6daec18236280e3b
제주 제주 <qwert6603@naver.com>
2022-01-07 08:43:19 +0900

    chapter2 array

--- chapter02.py
+++ chapter02.py
@@ -74,3 +74,26 @@ print(s[0:7:2])
 print(s[-4:-2])
 print(s[3:1])
 
+# len()
+x = [15, 64, 7, 3.14, [21,55], 'ABC']
+print(len(x))
+
+# 빈 배열 판단
+x = []
+if x:
+    print("not empty")
+else:
+    print("empty")
+
+# 배열의 대소 비교
+print([1,2,3] == [1,2,3])
+print([1,2,3] < [1,2,4])
+print([1,2,3,4] <= [1,2,3,4])
+print([1,2,3] < [1,2,3,4,5])
+print([1,2,3] < [1,2,3,5] < [1,2,3,5,6])
+print([1,2,4] < [1,2,3,4,5])
+
+# 내포
+numbers = [1,2,3,4,5]
+twise = [num * 2 for num in numbers if num % 2 == 1]
+print(twise)
